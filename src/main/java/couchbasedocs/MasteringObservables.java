92f7a6b6a4ce4e0bb672aebcc962dcd12ea63c3c
Bennett <bennett@yobennett.com>
2015-07-05 11:54:11 -0700

    couchbasedocs onError and take example

--- src/main/java/couchbasedocs/MasteringObservables.java
+++ src/main/java/couchbasedocs/MasteringObservables.java
@@ -2,6 +2,7 @@ package couchbasedocs;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action1;
 
 public class MasteringObservables {
 
@@ -27,9 +28,68 @@ public class MasteringObservables {
 
 	}
 
+	public static void testOnError() {
+
+		Observable.just(1, 2, 3)
+				.doOnNext(new Action1<Integer>() {
+					@Override
+					public void call(Integer integer) {
+						if (integer.equals(2)) {
+							throw new RuntimeException("2 sucks");
+						}
+					}
+				})
+				.subscribe(new Subscriber<Integer>() {
+					@Override
+					public void onCompleted() {
+						System.out.println("complete observable");
+					}
+
+					@Override
+					public void onError(Throwable throwable) {
+						System.out.println("error: " + throwable.getMessage());
+					}
+
+					@Override
+					public void onNext(Integer integer) {
+						System.out.println("got: "  + integer);
+					}
+				});
+
+	}
+
+	public static void take5() {
+
+		Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+				.take(5)
+				.subscribe(new Subscriber<Integer>() {
+					@Override
+					public void onCompleted() {
+						System.out.println("complete observable");
+					}
+
+					@Override
+					public void onError(Throwable throwable) {
+						System.out.println("error: " + throwable.getMessage());
+					}
+
+					@Override
+					public void onNext(Integer integer) {
+						System.out.println("got: "  + integer);
+					}
+				});
+
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Consume simple observable");
+		// simple observable
 		consumeObservable();
+
+		// test onError
+		testOnError();
+
+		// take(5)
+		take5();
 	}
 
 }
