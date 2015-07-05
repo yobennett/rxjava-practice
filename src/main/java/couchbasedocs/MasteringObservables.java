6233c268d2c2b5400900f56296b7d9a949d20473
Bennett <bennett@yobennett.com>
2015-07-05 15:37:09 -0700

    couchbasedocs error handling

--- src/main/java/couchbasedocs/MasteringObservables.java
+++ src/main/java/couchbasedocs/MasteringObservables.java
@@ -10,11 +10,13 @@ import rx.observables.BlockingObservable;
 import rx.observables.GroupedObservable;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class MasteringObservables {
 
+	final static Observable<Integer> example = Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 	final static Observable<Integer> evens = Observable.just(2, 4, 6, 8, 10);
 	final static Observable<Integer> odds = Observable.just(1, 3, 5, 7, 9);
 
@@ -278,9 +280,52 @@ public class MasteringObservables {
 				.subscribe(System.out::println);
 	}
 
+	public static void onErrorReturn() {
+		example
+				.doOnNext(integer -> {
+					throw new RuntimeException("Bah " + integer);
+				})
+				.onErrorReturn(throwable -> {
+					System.err.println("Oops. " + throwable.getMessage());
+					return 123;
+				})
+				.subscribe(System.out::println);
+	}
+
+	public static void retry() {
+		example
+				.doOnNext(integer -> {
+					if (new Random().nextInt(10) + 1 == 5) {
+						throw new RuntimeException("Boom!");
+					}
+				})
+				.retry()
+				.distinct()
+				.subscribe(System.out::println);
+	}
+
+	public static void retryWithBackoff() {
+
+		Observable
+				.range(1, 10)
+				.doOnNext(integer -> {
+					if (new Random().nextInt(10) + 1 == 5) {
+						throw new RuntimeException("Boom!");
+					}
+				})
+				.retryWhen(attempts ->
+						attempts.zipWith(Observable.range(1, 3), (n, i) -> i)
+						.flatMap(i -> {
+							System.out.println("delay retry by " + i + " second(s)");
+							return Observable.timer(i, TimeUnit.SECONDS);
+						}))
+				.distinct()
+				.subscribe(System.out::println);
+
+	}
 
 	public static void main(String[] args) throws Exception {
-		zip();
+		retryWithBackoff();
 	}
 
 }
